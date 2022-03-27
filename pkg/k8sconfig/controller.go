a4e86829f3d724533bdfcfb42ce9c32f8c35f12a
xiaolatiao <1628652790@qq.com>
2022-03-27 09:33:14 +0800

    feat:增加、更新、删除触发ingress配置重载、配合持久化

--- pkg/k8sconfig/controller.go
+++ pkg/k8sconfig/controller.go
@@ -2,8 +2,13 @@ package k8sconfig
 
 import (
 	"context"
-	"fmt"
+	"k8s.io/client-go/util/workqueue"
+	"log"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
+	"dierbei/operator-one/pkg/sysinit"
+
+	v1 "k8s.io/api/networking/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -17,22 +22,34 @@ func NewXltProxyController() *XltProxyController {
 }
 
 func (r *XltProxyController) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
-	//obj := &v1.Ingress{}
-	//if err := r.Get(ctx, req.NamespacedName, obj); err != nil {
-	//	return reconcile.Result{}, err
-	//}
-	//if v, ok := obj.Annotations["kubernetes.io/ingress.class"]; ok && v == "jtthink" {
-	//	fmt.Println(obj)
-	//}
-	//return reconcile.Result{}, nil
-
-	obj:=&Route{}
-	err:=r.Get(ctx,req.NamespacedName,obj)
-	if err!=nil{
+	ingress := &v1.Ingress{}
+	err := r.Get(ctx, req.NamespacedName, ingress)
+	if err != nil {
 		return reconcile.Result{}, err
 	}
-	fmt.Println(obj)
+	if r.IsJtProxy(ingress.Annotations) {
+		err = sysinit.ApplyConfig(ingress)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
 	return reconcile.Result{}, nil
+}
+
+//判断是否 是否 我们所需要处理的ingress
+func (r *XltProxyController) IsJtProxy(annotations map[string]string) bool {
+	if v, ok := annotations["kubernetes.io/ingress.class"]; ok && v == "jtthink" {
+		return true
+	}
+	return false
+}
+
+func (r *XltProxyController) OnDelete(event event.DeleteEvent, limitingInterface workqueue.RateLimitingInterface) {
+	if r.IsJtProxy(event.Object.GetAnnotations()) {
+		if err := sysinit.DeleteConfig(event.Object.GetName(), event.Object.GetNamespace()); err != nil {
+			log.Println(err)
+		}
+	}
 }
 
 func (r *XltProxyController) InjectClient(c client.Client) error {
--- test.go
+++ test.go
@@ -1,28 +1,33 @@
 package main
 
 import (
-	"dierbei/operator-one/pkg/filters"
-	"dierbei/operator-one/pkg/sysinit"
 	"fmt"
-	"github.com/valyala/fasthttp"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
 	"log"
 	"os"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 
+	"dierbei/operator-one/pkg/filters"
 	"dierbei/operator-one/pkg/k8sconfig"
+	"dierbei/operator-one/pkg/sysinit"
+
+	"github.com/valyala/fasthttp"
+	v1 "k8s.io/api/networking/v1"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
-func ProxyHandler(ctx *fasthttp.RequestCtx){
+func ProxyHandler(ctx *fasthttp.RequestCtx) {
 	//代表匹配到了 path
-	if getProxy:=sysinit.GetRoute(ctx.Request);getProxy!=nil{
-		filters.ProxyFilters(getProxy.RequestFilters).Do(ctx) //过滤
-		getProxy.Proxy.ServeHTTP(ctx) //反代
+	if getProxy := sysinit.GetRoute(ctx.Request); getProxy != nil {
+		filters.ProxyFilters(getProxy.RequestFilters).Do(ctx)  //过滤
+		getProxy.Proxy.ServeHTTP(ctx)                          //反代
 		filters.ProxyFilters(getProxy.ResponseFilters).Do(ctx) //过滤
-	}else{
+	} else {
 		ctx.Response.SetStatusCode(404)
 		ctx.Response.SetBodyString("404...")
 	}
@@ -38,38 +43,47 @@ func main() {
 	}
 
 	// crd
-	err=k8sconfig.SchemeBuilder.AddToScheme(mgr.GetScheme())
+	err = k8sconfig.SchemeBuilder.AddToScheme(mgr.GetScheme())
 	if err != nil {
 		mylog.Error(err, "unable add schema")
 		os.Exit(1)
 	}
-	err=builder.ControllerManagedBy(mgr).
-		For(&k8sconfig.Route{}).Complete(k8sconfig.NewXltProxyController())
-	if err != nil {
-		mylog.Error(err, "unable to create manager")
-		os.Exit(1)
-	}
-
-	//if err = builder.ControllerManagedBy(mgr).
-	//	For(&v1.Ingress{}).Complete(k8sconfig.NewXltProxyController()); err != nil {
+	//err=builder.ControllerManagedBy(mgr).
+	//	For(&k8sconfig.Route{}).Complete(k8sconfig.NewXltProxyController())
+	//if err != nil {
 	//	mylog.Error(err, "unable to create manager")
 	//	os.Exit(1)
 	//}
 
-	sysinit.InitConfig()  //初始化  业务系统配置
-	errCh:=make(chan error)
+	proxyCtl:=k8sconfig.NewXltProxyController()
+	err=builder.ControllerManagedBy(mgr).
+		For(&v1.Ingress{}).
+		Watches(&source.Kind{
+			Type: &v1.Ingress{},
+		},
+			handler.Funcs{DeleteFunc: proxyCtl.OnDelete}).
+		Complete(proxyCtl)
+
+	if err = builder.ControllerManagedBy(mgr).
+		For(&v1.Ingress{}).Complete(k8sconfig.NewXltProxyController()); err != nil {
+		mylog.Error(err, "unable to create manager")
+		os.Exit(1)
+	}
+
+	sysinit.InitConfig() //初始化  业务系统配置
+	errCh := make(chan error)
 	go func() {
 		//启动控制器管理器
-		if err=mgr.Start(signals.SetupSignalHandler());err!=nil{
-			errCh<-err
+		if err = mgr.Start(signals.SetupSignalHandler()); err != nil {
+			errCh <- err
 		}
 	}()
 	go func() {
 		// 启动网关
-		if err=fasthttp.ListenAndServe(fmt.Sprintf(":%d", sysinit.SysConfig.Server.Port),ProxyHandler);err!=nil{
-			errCh<-err
+		if err = fasthttp.ListenAndServe(fmt.Sprintf(":%d", sysinit.SysConfig.Server.Port), ProxyHandler); err != nil {
+			errCh <- err
 		}
 	}()
-	getError:=<-errCh
+	getError := <-errCh
 	log.Println(getError.Error())
 }
